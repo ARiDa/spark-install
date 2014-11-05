e862757b6890598a64afc8ed240b2070b07c3bea
Igo Brilhante <brilhanteigo@gmail.com>
2014-11-05 11:49:23 -0300

    improvements

--- install-spark-single-node.sh
+++ install-spark-single-node.sh
@@ -14,20 +14,20 @@ LOG=/tmp/spark_install.log
 
 function install_java_7() {
     echo "Checking Java 7"
-    if [`dpkg-query -W -f='${Status} ${Version}\n' openjdk-7-jdk | grep 'installed' | wc -l` -eq 0]; then
+    if [`$(dpkg-query -W -f='${Status} ${Version}\n' openjdk-7-jdk | grep 'installed' | wc -l)` -eq 0]; then
         echo "Installing Java 7"
         sudo apt-get update >> $LOG
         sudo apt-get install vim openjdk-7-jdk >> $LOG
     fi
 
-    if [`jps | grep  'Jps' | wc -l` -eq 1]; then
+    if [`$(jps | grep  'Jps' | wc -l)` -eq 1]; then
         echo "Java 7 installed"
-    if
+    fi
 }
 
 function download_spark() {
 
-    if [`ls $SPARK.tgz | wc -l` -eq 0 && `ls $SPARK | wc -l` -eq 0]; then
+    if [`$(ls $SPARK.tgz | wc -l` -eq 0 && `ls $SPARK | wc -l)` -eq 0]; then
         echo "Downloading "$SPARK
         wget http://d3kbcqa49mib13.cloudfront.net/$SPARK.tgz >> $LOG
         tar xzf $SPARK.tgz >> $LOG
@@ -47,7 +47,7 @@ function install_templates() {
     # default
     cat conf/spark-defaults.conf.template > conf/spark-defaults.conf
 
-    if [$1 -eq "m"]; then
+    if ["$1" -eq "m"]; then
         echo "SPARK_MASTER_IP="$SPARK_MASTER_IP >> conf/spark-env.sh
         echo "SPARK_WORKER_MEMORY="$SPARK_WORKER_MEMORY >> conf/spark-env.sh
         # slaves
